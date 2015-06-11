5fa87a0a84bf11c55deade85f949ea2b6c8c4113
zakihilmi <zaki_alhialy@yahoo.com>
2015-06-11 14:03:24 -0500

    associating number and boolean variables
    associating number and boolean variables

--- AssociatingJS/js/script.js
+++ AssociatingJS/js/script.js
@@ -1,2 +1,4 @@
-
-alert("this is zaki hilmi ");
+var age=31;   //number variable
+var isStudent=false;   //boolean variable
+var firstName="zaki"
+var phrase='I don\'t know if it\'s possible\n you\'ll fiqour that out
