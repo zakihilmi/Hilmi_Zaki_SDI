7ba0ad0f92c6e6edd590d47c5ec62f467333d79c
zakihilmi <zaki_alhialy@yahoo.com>
2015-06-11 18:47:02 -0500

    Creating declared & prompted height & width
    Creating declared & prompted height & width for a box to calculate the
    surface area and volume

---
+++ Hilmi_Zaki_Expressions_Assignment/javascript/script.js
@@ -0,0 +1,5 @@
+// We are calculating total area and volume of a box based on constraints given by users
+
+var height = prompt("we are calculating the area.\n please enter the height value");//we're declaring the height variable and giving a prompt for the value
+
+var width = prompt("please enter the width value");
