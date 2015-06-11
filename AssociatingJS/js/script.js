66490fd6f3014d33b69fa8a7aa7ce7ba28a595b4
zakihilmi <zaki_alhialy@yahoo.com>
2015-06-11 15:20:32 -0500

    Associating Arrays and console.log
    Adopting the set “zakiHilmi” consisting three text elements and
    applying console.log to define an element of that variable

--- AssociatingJS/js/script.js
+++ AssociatingJS/js/script.js
@@ -1,4 +1,7 @@
-var age=31;   //number variable
-var isStudent=false;   //boolean variable
-var firstName="zaki"
-var phrase='I don\'t know if it\'s possible\n you\'ll fiqour that out
+//Arrays
+
+var zakiHilmi = ["jack","rainbow","sothren"];
+
+
+zakiHilmi [1]="sothren";
+console.log (zakiHilmi[1]);
