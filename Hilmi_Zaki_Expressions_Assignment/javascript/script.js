207ba2de4cc3ac141ecebe4aa9f5b666f2de810d
zakihilmi <zaki_alhialy@yahoo.com>
2015-06-11 20:02:44 -0500

    The volume calculation added to the file
    The volume calculation added to the file

--- Hilmi_Zaki_Expressions_Assignment/javascript/script.js
+++ Hilmi_Zaki_Expressions_Assignment/javascript/script.js
@@ -1,10 +1,10 @@
 // We are calculating total area and volume of a box based on constraints given by the user
 
-var height = prompt("we are calculating the area.\n please enter the height value");//we're declaring the height variable and giving a prompt for the value
+var height = prompt("This is a process of calculating total area and volume of a box.\n please enter the height value");//we're declaring the height variable and giving a prompt for the value
 
 var width = prompt("please enter the width value");   // we declared the width
 
-var length = prompt("please enter the length");     //we declared the length
+var length = prompt("please enter the length value");     //we declared the length
 
 var areaofHeightandWidth= height * width;// we calculate the area of height and width by setting up this equation
 console.log(areaofHeightandWidth); // we save our value in console.log
@@ -17,6 +17,8 @@ console.log(areaofWidthandlength);
 
 var totalSurfaceArea = 2*(areaofHeightandWidth) + 2*(areaofHeightandLength) + 2*(areaofWidthandlength); // We figure out our total surface area by this equation
 console.log(totalSurfaceArea);
+
+var theVolume= height * width * length; // This is how to calculate the volume of the box
 
 
 
