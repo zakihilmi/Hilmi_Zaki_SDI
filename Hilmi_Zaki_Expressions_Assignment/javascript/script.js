663f04ada88b7ad7a1627b1c99e0bd86b9c83802
zakihilmi <zaki_alhialy@yahoo.com>
2015-06-11 20:30:17 -0500

    The final commit shows the alert demonstrates the resulted values for both total surface area and volume
    The final commit shows the alert demonstrates the resulted values for
    both total surface area and volume

--- Hilmi_Zaki_Expressions_Assignment/javascript/script.js
+++ Hilmi_Zaki_Expressions_Assignment/javascript/script.js
@@ -9,17 +9,19 @@ var length = prompt("please enter the length value");     //we declared the leng
 var areaofHeightandWidth= height * width;// we calculate the area of height and width by setting up this equation
 console.log(areaofHeightandWidth); // we save our value in console.log
 
-var areaofHeightandLength= height * length ; // we calculate the area of height and length by setting up this equation
-console.log(areaofHeightandLength);
+var areaofHeightandLength= height * length ;   // we calculate the area of height and length by setting up this equation
+console.log(areaofHeightandLength);     // we save the value for our variable
 
 var areaofWidthandlength = width * length; // This equation calculates the area of width and length
-console.log(areaofWidthandlength);
+console.log(areaofWidthandlength);      //we save the value for our variable
 
 var totalSurfaceArea = 2*(areaofHeightandWidth) + 2*(areaofHeightandLength) + 2*(areaofWidthandlength); // We figure out our total surface area by this equation
-console.log(totalSurfaceArea);
+console.log(totalSurfaceArea);      //we save the value for our variable
 
 var theVolume= height * width * length; // This is how to calculate the volume of the box
-
+console.log(theVolume);      //we save the value for our variable
+var result = "This is the total surface area" + " " +totalSurfaceArea + "cm2" + " "+"and this is the volume as calculated" + " " + theVolume + "cm3"; // This alert will be giving the final calculations for both (total of surface area and the volume of the box)
+alert(result );
 
 
 
