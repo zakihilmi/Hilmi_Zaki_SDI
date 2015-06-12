bef0302b6a2723bf2f6be291e442466aaa2e9ee1
zakihilmi <zaki_alhialy@yahoo.com>
2015-06-11 20:45:24 -0500

    The file is on repo site
    The file is on repo site

--- Hilmi_Zaki_Expressions_Assignment/javascript/script.js
+++ Hilmi_Zaki_Expressions_Assignment/javascript/script.js
@@ -1,3 +1,4 @@
+//Zaki Hilmi , June 11th 2015, Expressions
 // We are calculating total area and volume of a box based on constraints given by the user
 
 var height = prompt("This is a process of calculating total area and volume of a box.\n please enter the height value");//we're declaring the height variable and giving a prompt for the value
@@ -10,17 +11,18 @@ var areaofHeightandWidth= height * width;// we calculate the area of height and 
 console.log(areaofHeightandWidth); // we save our value in console.log
 
 var areaofHeightandLength= height * length ;   // we calculate the area of height and length by setting up this equation
-console.log(areaofHeightandLength);     // we save the value for our variable
+console.log(areaofHeightandLength);
 
 var areaofWidthandlength = width * length; // This equation calculates the area of width and length
 console.log(areaofWidthandlength);      //we save the value for our variable
 
 var totalSurfaceArea = 2*(areaofHeightandWidth) + 2*(areaofHeightandLength) + 2*(areaofWidthandlength); // We figure out our total surface area by this equation
-console.log(totalSurfaceArea);      //we save the value for our variable
+console.log(totalSurfaceArea);
 
 var theVolume= height * width * length; // This is how to calculate the volume of the box
-console.log(theVolume);      //we save the value for our variable
-var result = "This is the total surface area" + " " +totalSurfaceArea + "cm2" + " "+"and this is the volume as calculated" + " " + theVolume + "cm3"; // This alert will be giving the final calculations for both (total of surface area and the volume of the box)
+console.log(theVolume);
+var result = "This is the total surface area" + " " +totalSurfaceArea + "cm2" + " "+"and this is the volume as calculated" + " " + theVolume + "cm3";
+ // This alert will be giving the final calculations for both (total of surface area and the volume of the box)
 alert(result );
 
 
