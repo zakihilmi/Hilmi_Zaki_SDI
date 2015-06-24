eee72d3402139ed7e6910673120c3210132ceacd
zakihilmi <zaki_alhialy@yahoo.com>
2015-06-24 04:30:54 -0500

    the second function is anonymous and used to for lateral surface area calculation
    the second function is anonymous and used to for lateral surface area
    calculation

--- Hilmi_Zaki_Functions_Assignment /js/script.js
+++ Hilmi_Zaki_Functions_Assignment /js/script.js
@@ -4,12 +4,15 @@ var π =3.14 ;// This is a percentage required for the calculation
 var r=prompt("Please enter radius value! ");//we declared and prompted the radius
 var h =prompt("Please enter height value! ");//we declared and prompted the height
 
-var volum1=calcVolum(π,r,h);//setups a variable for our function
+var vol=calcVolum(π,r,h);//setups a variable for our function
 
-  function calcVolum(π,r,h){// our first function is to calculate the valume 
-      var volum = π * (r*r) * h;
-      return volum;
+  function calcVolum(π,r,h){// our first function is to calculate the valume
+      var volume = π * (r*r) * h;
+      return volume;
 
 }
-console.log(volum1);
+console.log(vol);
 
+var lateralSurface=function (π,r,h){// the second function will be anonymous and used to for lateral surface area calculation
+
+}
