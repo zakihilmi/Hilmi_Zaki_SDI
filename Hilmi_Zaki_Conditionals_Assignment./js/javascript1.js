0d4ba4fde847d7693390d5c0679074ab58a6134c
zakihilmi <zaki_alhialy@yahoo.com>
2015-06-24 03:21:22 -0500

    We created functions assignment folder &
    We created functions assignment folder & declared all required varibles

---
+++ Hilmi_Zaki_Conditionals_Assignment./js/javascript1.js
@@ -0,0 +1,95 @@
+// zaki Hilmi .17 June15, Conditionals assignment
+// This  process of a credit card eligibility , you must be 21 or over.
+// the annual income must be 18000 or over,minimum secured credit card must be 300 or over for in a month
+//other income will be post if available aim to
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//var age=0
+//var annualIncome=15000;
+//var miniSecuredCreditCard=400;
+//var otherIncome=0;
+
+
+//(age>=21&&annualIncome>=18000)?console.log("you are approved"):console.log("Sorry, you are not approved");
+  //  console.log(age>=21&&annualIncome>=18000);
+    alert("Congratulation, Your application is approved")
+//if(annualIncome>=18000||age>=21&&miniSecuredCreditCard>=300){
+    //console.log(annualIncome>=18000||age>=21&&miniSecuredCreditCard>=300)
+    alert("Congratulation, Your application is approved")
+//}else if(otherIncome>=600&&miniSecuredCreditCard>=300) {
+    //console.log("You are approved")
+//}else if
+
+//}else{console.log("you are not approve")}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//var age=21; console.log("please enter your age!");
+//if(age>21){console.log("you are approved for a credit card")
+//}else{console.log("We are sorry for being enable to approve your application at this time!")}
+//var annualIncome=20000; prompt("please enter your annual income!");
+//console.log(annualIncome);
+
+//var miniSecuredcreditcard=300;prompt("please enter your minimum Secured credit card!");
+//console.log(miniSecuredcreditcard);
+
+//var monthlyBounce=50;prompt("please enter any other outcomes");
+//console.log(monthlyBounce);
+//}else if(monthlyBounce){alert("you are approved for a credit card!")
+
+
+
---
+++ Hilmi_Zaki_Functions_Assignment /js/script.js
@@ -0,0 +1,6 @@
+//Zaki Hilmi 24June2015_Functions_Assignment
+//This is a process of calculating volume and surface area of a closed cylinder
+var π =3.14 ;// This is a percentage required for the calculation
+var r=0;prompt("Please enter radius value! ");//we declared and prompted the radius
+var h=0;prompt("Please enter height value! ");//we declared and prompted the height
+
