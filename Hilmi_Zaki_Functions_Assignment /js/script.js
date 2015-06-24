5d12f9b894acb76b3c30971f3f52bedb6ae92d4f
zakihilmi <zaki_alhialy@yahoo.com>
2015-06-24 04:13:01 -0500

    we set our function as defined it as an outside variable
    we set our first function(the value calculation),plus  we defined it as
    an outside variable

--- Hilmi_Zaki_Functions_Assignment /js/script.js
+++ Hilmi_Zaki_Functions_Assignment /js/script.js
@@ -1,6 +1,15 @@
 //Zaki Hilmi 24June2015_Functions_Assignment
 //This is a process of calculating volume and surface area of a closed cylinder
 var π =3.14 ;// This is a percentage required for the calculation
-var r=0;prompt("Please enter radius value! ");//we declared and prompted the radius
-var h=0;prompt("Please enter height value! ");//we declared and prompted the height
+var r=prompt("Please enter radius value! ");//we declared and prompted the radius
+var h =prompt("Please enter height value! ");//we declared and prompted the height
+
+var volum1=calcVolum(π,r,h);//setups a variable for our function
+
+  function calcVolum(π,r,h){// our first function is to calculate the valume 
+      var volum = π * (r*r) * h;
+      return volum;
+
+}
+console.log(volum1);
 
